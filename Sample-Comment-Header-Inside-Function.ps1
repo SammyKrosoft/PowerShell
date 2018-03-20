03076249d606ed5a088a66a0c53c21e6196e7a6c
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2018-03-20 17:54:37 -0400

    Create Sample-Comment-Header-Inside-Function.ps1

---
+++ Sample-Comment-Header-Inside-Function.ps1
@@ -0,0 +1,55 @@
+function Add-Extension
+{
+param ([string]$Name,[string]$Extension = "txt")
+$name = $name + "." + $extension
+$name
+
+<#
+.SYNOPSIS
+
+Adds a file name extension to a supplied name.
+
+.DESCRIPTION
+
+Adds a file name extension to a supplied name.
+Takes any strings for the file name or extension.
+
+.PARAMETER Name
+Specifies the file name.
+
+.PARAMETER Extension
+Specifies the extension. "Txt" is the default.
+
+.INPUTS
+
+None. You cannot pipe objects to Add-Extension.
+
+.OUTPUTS
+
+System.String. Add-Extension returns a string with the extension
+or file name.
+
+.EXAMPLE
+
+C:\PS> extension -name "File"
+File.txt
+
+.EXAMPLE
+
+C:\PS> extension -name "File" -extension "doc"
+File.doc
+
+.EXAMPLE
+
+C:\PS> extension "File" "doc"
+File.doc
+
+.LINK
+
+http://www.fabrikam.com/extension.html
+
+.LINK
+
+Set-Item
+#>
+}
