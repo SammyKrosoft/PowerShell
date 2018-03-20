f491ff7a8cff8fd75dbd0a0bc876692a2dd25763
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2018-03-20 18:07:50 -0400

    Create Another-Sample-Comments-Whole-Script.ps1

---
+++ Another-Sample-Comments-Whole-Script.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+
+Performs monthly data updates.
+
+.DESCRIPTION
+
+The Update-Month.ps1 script updates the registry with new data generated
+during the past month and generates a report.
+
+.PARAMETER InputPath
+Specifies the path to the CSV-based input file.
+
+.PARAMETER OutputPath
+Specifies the name and path for the CSV-based output file. By default,
+MonthlyUpdates.ps1 generates a name from the date and time it runs, and
+saves the output in the local directory.
+
+.INPUTS
+
+None. You cannot pipe objects to Update-Month.ps1.
+
+.OUTPUTS
+
+None. Update-Month.ps1 does not generate any output.
+
+.EXAMPLE
+
+C:\PS> .\Update-Month.ps1
+
+.EXAMPLE
+
+C:\PS> .\Update-Month.ps1 -inputpath C:\Data\January.csv
+
+.EXAMPLE
+
+C:\PS> .\Update-Month.ps1 -inputpath C:\Data\January.csv -outputPath `
+C:\Reports\2009\January.csv
+#>
+
+param ([string]$InputPath, [string]$OutPutPath)
+
+function Get-Data { 
+  #Your Code here
+}
