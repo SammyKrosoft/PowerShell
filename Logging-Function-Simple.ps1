cc71b5a87212ec3ae390c2bfd2786df562aaf2d3
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2018-03-20 22:03:51 -0400

    Create Logging-Function-Simple.ps1

---
+++ Logging-Function-Simple.ps1
@@ -0,0 +1,30 @@
+function Write-Log
+{
+	<#
+	.SYNOPSIS
+		This function creates or appends a line to a log file.
+
+	.PARAMETER  Message
+		The message parameter is the log message you'd like to record to the log file.
+
+	.EXAMPLE
+		PS C:\> Write-Log -Message 'Value1'
+		This example shows how to call the Write-Log function with named parameters.
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory)]
+		[string]$Message
+	)
+	
+	try
+	{
+		$DateTime = Get-Date -Format ‘MM-dd-yy HH:mm:ss’
+		$Invocation = "$($MyInvocation.MyCommand.Source | Split-Path -Leaf):$($MyInvocation.ScriptLineNumber)"
+		Add-Content -Value "$DateTime - $Invocation - $Message" -Path "$([environment]::GetEnvironmentVariable('TEMP', 'Machine'))\ScriptLog.log"
+	}
+	catch
+	{
+		Write-Error $_.Exception.Message
+	}
+}
