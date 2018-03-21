70369a7ed770435630d357f77b459373ac8c371f
sammykrosoft <sammy@hotmail.fr>
2018-03-21 10:11:49 -0400

    Change parts comments ...

--- Sam-Standard-Script-Header-Footer.ps1
+++ Sam-Standard-Script-Header-Footer.ps1
@@ -1,4 +1,4 @@
-<# SCRIPT_HEADER #>
+<# ---------------------------- SCRIPT_HEADER ---------------------------- #>
 #Initializing a $Stopwatch variable to use to measure script execution
 $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
 #Using Write-Debug and playing with $DebugPreference -> "Continue" will output whatever you put on Write-Debug "Your text/values"
@@ -12,25 +12,30 @@ $ScriptVersion = "1.0"
 $LogOrReportFile1 = "$PSScriptRoot\ReportOrLogFile_$(get-date -f yyyy-MM-dd-hh-mm-ss).csv"
 # Other Option for Log or report file definition (use one of these)
 $LogOrReportFile2 = "$((Get-Location).Path)\PowerShellScriptExecuted-$(Get-Date -Format 'dd-MMMM-yyyy-hh-mm-ss-tt').txt"
-<# /SCRIPT_HEADER #>
+<# ---------------------------- /SCRIPT_HEADER ---------------------------- #>
 
-<#----------------------------------------------------------[Declarations]----------------------------------------------------------#>
+<# DECLARATIONS #>
 #Your message
 $Message = "Hello world !"
 
-<#----------------------------------------------------------[Functions]----------------------------------------------------------#>
+<# /DECLARATIONS #>
+
+<# FUNCTIONS #>
 function Function1{
     param($FirstName="John",$LastName="Doe")
     Write-Host "Your full name is $FirstName $LastName ... can I call you $FirstName ?"
  }
 
-<#----------------------------------------------------------[Execution]----------------------------------------------------------#>
+ <# /FUNCTIONS #>
+
+<# EXECUTIONS #>
 Write-Debug $Message
 Function1
 
+<# /EXECUTIONS #>
 
-<# SCRIPT_FOOTER #>
+<# ---------------------------- SCRIPT_FOOTER ---------------------------- #>
 #Stopping StopWatch and report total elapsed time (TotalSeconds, TotalMilliseconds, TotalMinutes, etc...
 $stopwatch.Stop()
 Write-Host "The script took $($StopWatch.Elapsed.TotalSeconds) seconds to execute..."
-<# /SCRIPT_FOOTER #>
+<# ---------------------------- /SCRIPT_FOOTER (NOTHING BEYOND THIS POINT) ---------------------------- #>
