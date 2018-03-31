13d0037832dee9fe03c5efc2d4b77d2b7c359482
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2018-03-31 15:57:29 -0400

    Update Template-Sam-Standard-Script-Header-Footer.ps1

--- Template-Sam-Standard-Script-Header-Footer.ps1
+++ Template-Sam-Standard-Script-Header-Footer.ps1
@@ -8,40 +8,20 @@ $DebugPreference = "Continue"
 $ErrorPreference = "SilentlyContinue"
 #Script Version
 $ScriptVersion = "1.0"
-# Log or report file definition
-$LogOrReportFile1 = "$PSScriptRoot\ReportOrLogFile_$(get-date -f yyyy-MM-dd-hh-mm-ss).csv"
-# Other Option for Log or report file definition (use one of these)
-$LogOrReportFile2 = "$((Get-Location).Path)\PowerShellScriptExecuted-$(Get-Date -Format 'dd-MMMM-yyyy-hh-mm-ss-tt').txt"
+# Log or report file definition - dumping 2 examples, use both if you need to output a report AND a script execution Log
+# or just use one (delete the unused)
+$CSVReportFile = "$PSScriptRoot\ReportOrLogFile_$(get-date -f yyyy-MM-dd-hh-mm-ss).csv"
+$ScriptExecutionLogReportFile = "$((Get-Location).Path)\PowerShellScriptExecuted-$(Get-Date -Format 'dd-MMMM-yyyy-hh-mm-ss-tt').txt"
 <# ---------------------------- /SCRIPT_HEADER ---------------------------- #>
-
-
-
 <# DECLARATIONS #>
 #Your message
 $Message = "Hello world !"
-
-
 <# /DECLARATIONS #>
-
-
-
 <# FUNCTIONS #>
-function Function1{
-    param($FirstName="John",$LastName="Doe")
-    Write-Host "Your full name is $FirstName $LastName ... can I call you $FirstName ?"
- }
-
- <# /FUNCTIONS #>
-
-
-
-
+<# /FUNCTIONS #>
 <# EXECUTIONS #>
 Write-Debug $Message
-Function1
-
 <# /EXECUTIONS #>
-
 <# ---------------------------- SCRIPT_FOOTER ---------------------------- #>
 #Stopping StopWatch and report total elapsed time (TotalSeconds, TotalMilliseconds, TotalMinutes, etc...
 $stopwatch.Stop()
