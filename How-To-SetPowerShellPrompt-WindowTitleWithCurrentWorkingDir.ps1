690b4a3ba4327c6280eef5c9e20142af93196345
Sammy Krosoft <sammy@hotmail.fr>
2018-04-14 13:21:53 -0400

    Completed the comment based help section

--- How-To-SetPowerShellPrompt-WindowTitleWithCurrentWorkingDir.ps1
+++ How-To-SetPowerShellPrompt-WindowTitleWithCurrentWorkingDir.ps1
@@ -12,13 +12,18 @@
     you change your working location - that is possible thanks to that PowerShell "Prompt" built-in function that
     you can customize to whatever you want it to be !
 
-    As mentionned above, the PowerShell system function called "Prompt" is a function that is called automatically 
+    As mentionned above, the PowerShell Built-In function called "Prompt" is a function that is called automatically 
     everytime you hit "Enter" on a PowerShell host. Setting the Prompt function to get the current directoy or 
     PS Drive location and update the current PowerShell window will update your current window with the current
     directory or PS Drive location everytime you do a "CD <Directory or PSDrive>".
 
     In the sample prompt here we are using $Host.UI.RawUI.WindowTitle to change the current PowerShell Window
     title, and we follow the function definition with the "PS>" string.
+
+    See the Related Links section for the URL to the PowerShell prompt built-in function, or to open directly the Microsoft
+    documentation about the Prompt, just type
+    
+        Get-Help .\This-Script -Online
 
     
 .INPUTS
@@ -28,18 +33,23 @@
     None.
 
 .EXAMPLE
-    Function prompt {$Host.UI.RawUI.WindowTitle = $(Get-Location);"PS>"}
-    This will set the Window title to the current location ($(Get-Location)) and will also set the user prompt to "PS>"
-
-.EXAMPLE
     Function Prompt {"PS>"}
     This will just set the user prompt to "PS>"
 
+
+.EXAMPLE
+    Function prompt {$Host.UI.RawUI.WindowTitle = $(Get-Location);"PS>"}
+    This will set the Window title to the current location ($(Get-Location)) and will also set the user prompt to "PS>".
+    Note the 2 commands inside the curly brackets, separated by a ";"
+    
 .NOTES
     None
 
 .LINK
-    
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts
+
 
 #>
+
+
 function prompt {$Host.UI.RawUI.WindowTitle = $(Get-Location);"PS>"}
