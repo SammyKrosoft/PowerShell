4038729ed82d227656999ebb98a15074d0e404e6
Sammy Krosoft <sammy@hotmail.fr>
2018-05-09 22:44:12 -0400

    commit

--- Get-EventsFromEventLog/Get-EventsFromEventLogspe.ps1
+++ Get-EventsFromEventLog/Get-EventsFromEventLogspe.ps1
@@ -458,5 +458,7 @@ If ($ExportToFile){
 <# ---------------------------- SCRIPT_FOOTER ---------------------------- #>
 #Stopping StopWatch and report total elapsed time (TotalSeconds, TotalMilliseconds, TotalMinutes, etc...)
 $stopwatch.Stop()
-Write-Host "The script took $($StopWatch.Elapsed.TotalSeconds) seconds to execute..."
+$msg = "The script took $($StopWatch.Elapsed.TotalSeconds) seconds to execute..."
+Write-Host $msg
+Say $msg
 <# ---------------- /SCRIPT_FOOTER (NOTHING BEYOND THIS POINT) ----------- #>
