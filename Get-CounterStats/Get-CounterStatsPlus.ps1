732794a397a9c07dfa0de7782912ab04a0d9b389
unknown <samdrey@microsoft.com>
2018-05-11 14:08:30 -0400

    commit

--- Get-CounterStats/Get-CounterStatsPlus.ps1
+++ Get-CounterStats/Get-CounterStatsPlus.ps1
@@ -168,13 +168,13 @@ function Get-CounterStats {
 
 $Counter = @"
 Processor(_total)\% processor time 
-\MSExchange RpcClientAccess\RPC Averaged Latency
-\MSExchangeIS Store(*)\RPC Average Latency
-\MSExchange RpcClientAccess\RPC Requests
+MSExchange RpcClientAccess\RPC Averaged Latency
+MSExchangeIS Store(*)\RPC Average Latency
+MSExchange RpcClientAccess\RPC Requests
 Memory\Available MBytes 
 PhysicalDisk(*)\Avg. Disk sec/Transfer 
 Network Interface(*)\Bytes Total/sec
-\MSExchangeTransport Queues(*)\Submission Queue Length
+MSExchangeTransport Queues(*)\Submission Queue Length
 "@ 
 
     (Get-Counter -ComputerName $ComputerName -Counter (Convert-HString -HString $Counter)).counterSamples | ForEach-Object {
@@ -238,7 +238,8 @@ Write-Host "Gathering performance counters for $($Servers -Join ", ")"
 Write-Host "That's a total of $($Servers.count) servers"
 
 #Collecting counter information for target servers
-$Expression = ("Get-CounterStats -ComputerName $Servers | Select-Object computerName,datetime,") + (If ($IncludeFullCounterPath) {"WholeCounter,"}Else{""}) + ("CounterCategory,CounterName,Instance,Value | Export-Csv -Path $OutputFile -Append -NoTypeInformation")
+#$Expression = ("Get-CounterStats -ComputerName $Servers | Select-Object computerName,datetime,") + $({If ($IncludeFullCounterPath) {"WholeCounter,"}Else{""}}) + ("CounterCategory,CounterName,Instance,Value | Export-Csv -Path $OutputFile -Append -NoTypeInformation")
+$Expression = "Get-CounterStats -ComputerName $Servers | Select-Object computerName,datetime,CounterCategory,CounterName,Instance,Value | Export-Csv -Path $OutputFile -Append -NoTypeInformation"
 For ($ReRun = 1;$ReRun -le $NumberOfSamples;$ReRun ++){
     Write-Progress -Id 1 -Activity "Gathering $NumberOfSamples counters" -Status "Sample $ReRun of $NumberOfSamples" -PercentComplete ($($rerun/$NumberOfSamples*100))
     invoke-expression $Expression
