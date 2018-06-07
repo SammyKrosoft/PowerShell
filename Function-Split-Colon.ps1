9e8dac107d5b85a97e9d3b90e824d87f5dde43c3
unknown <samdrey@microsoft.com>
2018-06-06 23:04:03 -0400

    update

---
+++ Function-Split-Colon.ps1
@@ -0,0 +1,16 @@
+﻿Function Split-ListColon {
+    param(
+        [string]$StringToSplit,
+        [switch]$Noquotes
+    )
+    $TargetSplit = $StringToSplit.Split(',')
+    $ListItems = ""
+    If ($NoQuotes){
+        For ($i = 0; $i -lt $TargetSplit.Count - 1; $i++) {$ListItems += $TargetSplit[$i].trim() + (", ")}
+        $ListItems += $TargetSplit[$TargetSplit.Count - 1].trim()
+    } Else {
+        For ($i = 0; $i -lt $TargetSplit.Count - 1; $i++) {$ListItems += ("""") + $TargetSplit[$i].trim() + (""", ")}
+        $ListItems += ("""") + $TargetSplit[$TargetSplit.Count - 1].trim() + ("""")
+    }
+    Return $ListItems
+}
