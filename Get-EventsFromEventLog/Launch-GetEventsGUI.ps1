9b469a056053996cb616af846dd6534ded1cee7c
unknown <samdrey@microsoft.com>
2018-06-06 22:29:42 -0400

    fix Launch-GetEventsGUI

--- Get-EventsFromEventLog/Launch-GetEventsGUI.ps1
+++ Get-EventsFromEventLog/Launch-GetEventsGUI.ps1
@@ -9,6 +9,28 @@ $global:GUIversion = "1.1"
 #region Functions definitions (NOT the WPF form events)
 #========================================================
 
+Function IsPSV3 {
+    <#
+    .DESCRIPTION
+    Just printing Powershell version and returning "true" if powershell version
+    is Powershell v3 or more recent, and "false" if it's version 2.
+    .OUTPUTS
+    Returns $true or $false
+    .EXAMPLE
+    IsPSVersionV3
+    #>
+    $PowerShellMajorVersion = $PSVersionTable.PSVersion.Major
+    $msgPowershellMajorVersion = "You're running Powershell v$PowerShellMajorVersion"
+    Write-Host $msgPowershellMajorVersion -BackgroundColor blue -ForegroundColor yellow
+    If($PowerShellMajorVersion -le 2){
+        Write-Host "Sorry, PowerShell v3 or more is required. Exiting."
+        Return $false
+        Exit
+    } Else {
+        Return $true
+        }
+}
+
 Function Get-EventsFromEventLogs {
         <#
     .SYNOPSIS
