14e4c46b9f22cd55efca4b1601fd6b6f13bc9866
Sammy Krosoft <sammy@hotmail.fr>
2018-06-19 23:17:52 -0400

    update WPF method

--- How-To-Load-WPF-From-XAML.ps1
+++ How-To-Load-WPF-From-XAML.ps1
@@ -3,8 +3,13 @@
 Add-Type -AssemblyName presentationframework, presentationcore
 $wpf = @{ }
 # NOTE: Either load from a XAML file or paste the XAML file content in a "Here String"
-$inputXML = Get-Content -Path ".\WPFGUIinTenLines\MainWindow.xaml"
-# or $inputXML = @"Here-String pasted from XAML"@
+#$inputXML = Get-Content -Path ".\WPFGUIinTenLines\MainWindow.xaml"
+$inputXML = @"
+
+Here-String pasted from XAML
+
+"@
+
 $inputXMLClean = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace 'x:Class=".*?"','' -replace 'd:DesignHeight="\d*?"','' -replace 'd:DesignWidth="\d*?"',''
 [xml]$xaml = $inputXMLClean
 $reader = New-Object System.Xml.XmlNodeReader $xaml
@@ -13,4 +18,10 @@ $namedNodes = $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Nam
 $namedNodes | ForEach-Object {$wpf.Add($_.Name, $tempform.FindName($_.Name))}
 
 # Load the form:
-$wpf.GrabEventLogs.ShowDialog() | Out-Null
+# Older way >>>>> $wpf.MyFormName.ShowDialog() | Out-Null >>>>> generates crash if run multiple times
+# Newer way >>>>> avoiding crashes after a couple of launches in PowerShell...
+# USing method from https://gist.github.com/altrive/6227237 to avoid crashing Powershell after we re-run the script after some inactivity time or if we run it several times consecutively...
+$async = $wpf.MyFormName.Dispatcher.InvokeAsync({
+    $wpf.MyFormName.ShowDialog() | Out-Null
+})
+$async.Wait() | Out-Null
---
+++ Test-WPFDataGrid.ps1
@@ -0,0 +1,20 @@
+
+
+$wpf = @"
+
+<Window x:Class="simple_datagrid_tests.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:simple_datagrid_tests"
+        mc:Ignorable="d"
+        Title="MainWindow" Height="450" Width="800">
+    <Grid>
+        <DataGrid x:Name="dataGrid" HorizontalAlignment="Left" Height="402" VerticalAlignment="Top" Width="775" Margin="10,10,0,0"/>
+
+    </Grid>
+</Window>
+
+"@
+
