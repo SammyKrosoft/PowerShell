f99fdc77c90e3c5a296244c58bda00c269c601fb
unknown <samdrey@microsoft.com>
2018-07-05 17:09:43 -0400

    Catching up with PowerShell

---
+++ Function-WPFMessageBox.ps1
@@ -0,0 +1,6 @@
+﻿Function MessageBox ($Title = "Validation",$msg="Usage: MessageBox -Title ""Title"" -msg ""Your message"" -Button ""Ok/OkCancel/YesNo"" -Icon ""Question/Information/Error/Warning""",$Button = "YesNo",$Icon = "Question") {
+    return [System.Windows.MessageBox]::Show($msg,$Title, $Button, $icon)
+}
+
+Add-Type -AssemblyName presentationframework, presentationcore
+MessageBox
