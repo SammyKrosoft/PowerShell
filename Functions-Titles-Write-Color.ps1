07ce6c4c7b16fb6e3aa59b26a96ad8304377020c
unknown <samdrey@microsoft.com>
2018-09-13 14:24:20 -0400

    New Functions

---
+++ Functions-Titles-Write-Color.ps1
@@ -0,0 +1,33 @@
+﻿Function Title1 ($title, $TotalLength = 100, $Back = "Yellow", $Fore = "Black") {
+    $TitleLength = $Title.Length
+    [string]$StarsBeforeAndAfter = ""
+    $RemainingLength = $TotalLength - $TitleLength
+    If ($($RemainingLength % 2) -ne 0) {
+        $Title = $Title + " "
+    }
+    $Counter = 0
+    For ($i=1;$i -le $(($RemainingLength)/2);$i++) {
+        $StarsBeforeAndAfter += "*"
+        $counter++
+    }
+    
+    $Title = $StarsBeforeAndAfter + $Title + $StarsBeforeAndAfter
+    Write-Host $Title -BackgroundColor $Back -foregroundcolor $Fore
+    Write-Host
+    
+}
+Function LogRed ($Message){
+    Write-Host $message -ForegroundColor Red
+}
+
+Function LogGreen ($message){
+    Write-Host $message -ForegroundColor Green
+}
+
+Function LogYellow ($message){
+    Write-Host $message -ForegroundColor Yellow
+}
+
+Function LogBlue ($message){
+    Write-Host $message -ForegroundColor Blue
+}
