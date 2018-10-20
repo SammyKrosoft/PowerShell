d64d5e64e1cd678c5cb8226b486ca86873cf4b37
unknown <samdrey@microsoft.com>
2018-10-19 23:21:44 -0400

    fix bug

--- Start-E2016ServerComponentStateToActive.ps1
+++ Start-E2016ServerComponentStateToActive.ps1
@@ -23,68 +23,69 @@ PS> .\SetExchangeComponentToActive.ps1 -Server Server1
 #)
 
 Function Test-ExchTools(){
-<#
-.SYNOPSIS
-This small function will just check if you have Exchange tools installed or available on the
-current PowerShell session.
-
-.DESCRIPTION
-The presence of Exchange tools are checked by trying to execute "Get-ExBanner", one of the basic Exchange
-cmdlets that runs when the Exchange Management Shell is called.
-
-Just use Test-ExchTools in your script to make the script exit if not launched from an Exchange
-tools PowerShell session...
-
-.EXAMPLE
-Test-ExchTools
-=> will exit the script/program si Exchange tools are not installed
-#>
-    Try
-    {
-        #Get-command Get-ExBanner -ErrorAction Stop
-        Get-command Get-Mailbox -ErrorAction Stop
-        $ExchInstalledStatus = $true
-        $Message = "Exchange tools are present !"
-        Write-Host $Message -ForegroundColor Blue -BackgroundColor Red
+    <#
+    .SYNOPSIS
+    This small function will just check if you have Exchange tools installed or available on the
+    current PowerShell session.
+    
+    .DESCRIPTION
+    The presence of Exchange tools are checked by trying to execute "Get-ExBanner", one of the basic Exchange
+    cmdlets that runs when the Exchange Management Shell is called.
+    
+    Just use Test-ExchTools in your script to make the script exit if not launched from an Exchange
+    tools PowerShell session...
+    
+    .EXAMPLE
+    Test-ExchTools
+    => will exit the script/program si Exchange tools are not installed
+    #>
+        Try
+        {
+            #Get-command Get-ExBanner -ErrorAction Stop
+            Get-command Get-Mailbox -ErrorAction Stop
+            $ExchInstalledStatus = $true
+            $Message = "Exchange tools are present !"
+            Write-Host $Message -ForegroundColor Blue -BackgroundColor Red
+        }
+        Catch [System.SystemException]
+        {
+            $ExchInstalledStatus = $false
+            $Message = "Exchange Tools are not present ! This script/tool need these. Exiting..."
+            Write-Host $Message -ForegroundColor red -BackgroundColor Blue
+            Exit
+        }
+        Return $ExchInstalledStatus
     }
-    Catch [System.SystemException]
-    {
-        $ExchInstalledStatus = $false
-        $Message = "Exchange Tools are not present ! This script/tool need these. Exiting..."
-        Write-Host $Message -ForegroundColor red -BackgroundColor Blue
-        Exit
+    
+    If (!(Test-ExchTools)){exit}
+    
+    cls
+    $E2016NamesList = @()
+    $E2016 = Get-ExchangeServer | ? {$_.AdminDisplayVersion -match "15.1"} 
+    Foreach ($item in $E2016){$E2016NamesList += $($item.Name)}
+    
+    $counter = 0
+    Foreach ($Server in $E2016){
+    write-progress -id 1 -Activity "Activating all components" -Status "Server $Server" -PercentComplete $($Counter/$($E2016.Count)*100)
+    $Counter++
+    
+    #Designates that the server is out of maintenance mode
+    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "setting ServerWideOffline..." -PercentComplete 0
+    Set-ServerComponentState $($Server.Name) -Component ServerWideOffline -State Active -Requester Functional
+    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "ServerWideOffline done ... now setting Monitoring" -PercentComplete 33
+    Set-ServerComponentState $($Server.Name) -Component Monitoring -State Active -Requester Functional
+    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "Monitoring done ... now setting RecoveryACtionsEnabled" -PercentComplete 66
+    Set-ServerComponentState $($Server.Name) -Component RecoveryActionsEnabled -State Active -Requester Functional
+    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "All 3 components set !" -PercentComplete 100
+    
+    #Get the status of component 
+    $ComponentStateStatus = Get-ServerComponentState $Server 
+    $ComponentStateStatus | ft Component,State -Autosize
+    
+    
+    Write-Host "$Server is now completely out of maintenance mode and component are active and functional." -ForegroundColor Yellow
+    
     }
-    Return $ExchInstalledStatus
-}
-
-If (!Test-ExchTools){exit}
-
-cls
-$E2016NamesList = @()
-$E2016 = Get-ExchangeServer | ? {$_.AdminDisplayVersion -match "15.1"} 
-Foreach ($item in $E2016){$E2016NamesList += $($item.Name)}
-
-$counter = 0
-Foreach ($Server in $E2016){
-write-progress -id 1 -Activity "Activating all components" -Status "Server $Server" -PercentComplete $($Counter/$($E2016.Count)*100)
-$Counter++
-
-#Designates that the server is out of maintenance mode
-Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "setting ServerWideOffline..." -PercentComplete 0
-Set-ServerComponentState $($Server.Name) -Component ServerWideOffline -State Active -Requester Functional
-Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "ServerWideOffline done ... now setting Monitoring" -PercentComplete 33
-Set-ServerComponentState $($Server.Name) -Component Monitoring -State Active -Requester Functional
-Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "Monitoring done ... now setting RecoveryACtionsEnabled" -PercentComplete 66
-Set-ServerComponentState $($Server.Name) -Component RecoveryActionsEnabled -State Active -Requester Functional
-Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "All 3 components set !" -PercentComplete 100
-
-#Get the status of component 
-$ComponentStateStatus = Get-ServerComponentState $Server 
-$ComponentStateStatus | ft Component,State -Autosize
-
-
-Write-Host "$Server is now completely out of maintenance mode and component are active and functional." -ForegroundColor Yellow
-
-}
-write-progress -id 1 -Activity "Activating all components" -Status "All done !" -PercentComplete $($Counter/$($E2016.Count)*100)
-sleep 1
+    write-progress -id 1 -Activity "Activating all components" -Status "All done !" -PercentComplete $($Counter/$($E2016.Count)*100)
+    sleep 1
+    
