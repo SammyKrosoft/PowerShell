91d1bd1f981951983b0c02d6f0f00d096bad18e3
Sammy Krosoft <sammy@hotmail.fr>
2018-10-20 23:00:44 -0400

    change a bit the logic

--- Start-E2016ServerComponentStateToActive.ps1
+++ Start-E2016ServerComponentStateToActive.ps1
@@ -39,51 +39,69 @@ Function Test-ExchTools(){
     Test-ExchTools
     => will exit the script/program si Exchange tools are not installed
     #>
-        Try
-        {
-            Get-command Get-MAilbox -ErrorAction Stop
-            $ExchInstalledStatus = $true
-            $Message = "Exchange tools are present !"
-            Write-Host $Message -ForegroundColor Blue -BackgroundColor Red
-        }
-        Catch [System.SystemException]
-        {
-            $ExchInstalledStatus = $false
-            $Message = "Exchange Tools are not present ! This script/tool need these. Exiting..."
-            Write-Host $Message -ForegroundColor red -BackgroundColor Blue
-            Exit
-        }
-        Return $ExchInstalledStatus
+    Try
+    {
+        Get-command Get-MAilbox -ErrorAction Stop
+        $ExchInstalledStatus = $true
+        $Message = "Exchange tools are present !"
+        Write-Host $Message -ForegroundColor Blue -BackgroundColor Red
     }
+    Catch [System.SystemException]
+    {
+        $ExchInstalledStatus = $false
+        $Message = "Exchange Tools are not present ! This script/tool need these. Exiting..."
+        Write-Host $Message -ForegroundColor red -BackgroundColor Blue
+        Exit
+    }
+    Return $ExchInstalledStatus
+ }
 
-    If (!(Test-ExchTools)){exit}
+If (!(Test-ExchTools)){exit}
 
-    cls
-    $E2016NamesList = @()
-    $E2016 = Get-ExchangeServer | ? {$_.AdminDisplayVersion -match "15.1"} 
-    Foreach ($item in $E2016){$E2016NamesList += $($item.Name)}
+cls
+$E2016NamesList = @()
+$E2016 = Get-ExchangeServer | ? {$_.AdminDisplayVersion -match "15.1"} 
+Foreach ($item in $E2016){$E2016NamesList += $($item.Name)}
 
-    $counter = 0
-    Foreach ($Server in $E2016){
+$counter = 0
+Foreach ($Server in $E2016){
     write-progress -id 1 -Activity "Activating all components" -Status "Server $Server" -PercentComplete $($Counter/$($E2016.Count)*100)
     $Counter++
 
-    #Designates that the server is out of maintenance mode
-    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "setting ServerWideOffline..." -PercentComplete 0
-    Set-ServerComponentState $($Server.Name) -Component ServerWideOffline -State Active -Requester Functional
-    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "ServerWideOffline done ... now setting Monitoring" -PercentComplete 33
-    Set-ServerComponentState $($Server.Name) -Component Monitoring -State Active -Requester Functional
-    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "Monitoring done ... now setting RecoveryACtionsEnabled" -PercentComplete 66
-    Set-ServerComponentState $($Server.Name) -Component RecoveryActionsEnabled -State Active -Requester Functional
-    Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "All 3 components set !" -PercentComplete 100
-
     #Get the status of component 
     $ComponentStateStatus = Get-ServerComponentState ($Server.Name) 
-    $ComponentStateStatus | ft Component,State -Autosize
+    #$ComponentStateStatus | ft Component,State -Autosize
+    $ACtiveComponents = ($ComponentStateStatus | ? {$_.State -eq "Active"}).count
+    $InactiveComponents = ($ComponentStateStatus | ? {$_.State -eq "Inactive"}).count
 
+    Write-Host "There are $ACtiveComponents active compponents, and $InactiveComponents inactive components"
 
-    Write-Host "$Server is now completely out of maintenance mode and component are active and functional." -ForegroundColor Yellow
-
+    If ($InactiveComponents -le 2){
+        Write-Host "There are only $InactiveComponents, everything looks good ... here are the list of components:"
+        $ComponentStateStatus | ft Component,State -Autosize
+        Continue
+    } Else {
+        Write-host "More than 2 components are not active :"
+        $ComponentStateStatus | ? {$_.State -eq "Active"}
+        Write-host "... trying to re-activate all components..." 
+        #Designates that the server is out of maintenance mode
+        Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "setting ServerWideOffline..." -PercentComplete 0
+        Set-ServerComponentState $($Server.Name) -Component ServerWideOffline -State Active -Requester Functional
+        Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "ServerWideOffline done ... now setting Monitoring" -PercentComplete 33
+        Set-ServerComponentState $($Server.Name) -Component Monitoring -State Active -Requester Functional
+        Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "Monitoring done ... now setting RecoveryACtionsEnabled" -PercentComplete 66
+        Set-ServerComponentState $($Server.Name) -Component RecoveryActionsEnabled -State Active -Requester Functional
+        Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "All 3 components set !" -PercentComplete 100
     }
-    write-progress -id 1 -Activity "Activating all components" -Status "All done !" -PercentComplete $($Counter/$($E2016.Count)*100)
-    sleep 1
+    #Get the status of components
+    $ComponentStateStatus = Get-ServerComponentState ($Server.Name) 
+    $ComponentStateStatus | ft Component,State -Autosize
+    $ACtiveComponents = ($ComponentStateStatus | ? {$_.State -eq "Active"}).count
+    $InactiveComponents = ($ComponentStateStatus | ? {$_.State -eq "Inactive"}).count
+
+    Write-Host "There are now $ACtiveComponents active compponents, and $InactiveComponents inactive components"
+    If ($InactiveComponents -lt 2) {Write-host "There are still some inactive components ... please troubleshoot !" -BackgroundColor Red -ForegroundColor Yellow} Else {Write-Host "$Server is now completely out of maintenance mode and component are active and functional." -ForegroundColor Yellow}
+}
+
+write-progress -id 1 -Activity "Activating all components" -Status "All done !" -PercentComplete $($Counter/$($E2016.Count)*100)
+sleep 1
