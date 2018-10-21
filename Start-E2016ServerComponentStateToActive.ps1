61610cc0ee4d232aee01e0e195dec4fed03a28b4
Sammy Krosoft <sammy@hotmail.fr>
2018-10-20 20:07:19 -0400

    removed spaces

--- Start-E2016ServerComponentStateToActive.ps1
+++ Start-E2016ServerComponentStateToActive.ps1
@@ -27,14 +27,14 @@ Function Test-ExchTools(){
     .SYNOPSIS
     This small function will just check if you have Exchange tools installed or available on the
     current PowerShell session.
-    
+
     .DESCRIPTION
     The presence of Exchange tools are checked by trying to execute "Get-ExBanner", one of the basic Exchange
     cmdlets that runs when the Exchange Management Shell is called.
-    
+
     Just use Test-ExchTools in your script to make the script exit if not launched from an Exchange
     tools PowerShell session...
-    
+
     .EXAMPLE
     Test-ExchTools
     => will exit the script/program si Exchange tools are not installed
@@ -55,19 +55,19 @@ Function Test-ExchTools(){
         }
         Return $ExchInstalledStatus
     }
-    
+
     If (!(Test-ExchTools)){exit}
-    
+
     cls
     $E2016NamesList = @()
     $E2016 = Get-ExchangeServer | ? {$_.AdminDisplayVersion -match "15.1"} 
     Foreach ($item in $E2016){$E2016NamesList += $($item.Name)}
-    
+
     $counter = 0
     Foreach ($Server in $E2016){
     write-progress -id 1 -Activity "Activating all components" -Status "Server $Server" -PercentComplete $($Counter/$($E2016.Count)*100)
     $Counter++
-    
+
     #Designates that the server is out of maintenance mode
     Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "setting ServerWideOffline..." -PercentComplete 0
     Set-ServerComponentState $($Server.Name) -Component ServerWideOffline -State Active -Requester Functional
@@ -76,15 +76,14 @@ Function Test-ExchTools(){
     Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "Monitoring done ... now setting RecoveryACtionsEnabled" -PercentComplete 66
     Set-ServerComponentState $($Server.Name) -Component RecoveryActionsEnabled -State Active -Requester Functional
     Write-progress -id 2 -ParentId 1 -Activity "Setting component states" -Status "All 3 components set !" -PercentComplete 100
-    
+
     #Get the status of component 
     $ComponentStateStatus = Get-ServerComponentState ($Server.Name) 
     $ComponentStateStatus | ft Component,State -Autosize
-    
-    
+
+
     Write-Host "$Server is now completely out of maintenance mode and component are active and functional." -ForegroundColor Yellow
-    
+
     }
     write-progress -id 1 -Activity "Activating all components" -Status "All done !" -PercentComplete $($Counter/$($E2016.Count)*100)
     sleep 1
-    
