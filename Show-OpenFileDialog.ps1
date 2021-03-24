0f34ac2c0f705f5792dab02305d11970c7eb9510
Sammy Krosoft <sammy@hotmail.fr>
2021-03-23 22:47:17 -0400

    forgot to load PresentationFramework asembly

--- Show-OpenFileDialog.ps1
+++ Show-OpenFileDialog.ps1
@@ -1,9 +1,21 @@
-function Show-OpenFileDialog
-{
+function Show-OpenFileDialog {
+<#
+.DESCRIPTION
+    This function is a PowerShell call to the OpenFileDialog box using WPF.
+    We'll see many examples of OpenFileDialog using Windows Forms, but we'll try to
+    get off Windows Forms as it's legacy tech.
+
+.LINK
+    https://docs.microsoft.com/en-us/dotnet/api/microsoft.win32?view=net-5.0
+
+#>
+
     param
     ($Title = 'Select a file to use', $Filter = 'Comma Separated|*.csv|Text|*.txt',$InitialDirectory = "c:\temp")
+    
+    Add-Type -AssemblyName PresentationFramework
 
-    $dialog = New-Object -TypeName Microsoft.Win32.OpenFileDialog
+    $dialog = New-Object -TypeName 'Microsoft.Win32.OpenFileDialog'
     $dialog.Title = $Title
     $dialog.Filter = $Filter
     If (!(Test-Path $InitialDirectory)){$InitialDirectory = "$($env:Userprofile)\Documents"} #If the default C:\temp doesn't exist, defaults to user's Document folder
