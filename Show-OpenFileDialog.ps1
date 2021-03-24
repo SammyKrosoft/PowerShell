27bc1629bdddbad6349a14825fcb71b137f56186
Sammy Krosoft <sammy@hotmail.fr>
2021-03-23 23:31:44 -0400

    update

--- Show-OpenFileDialog.ps1
+++ Show-OpenFileDialog.ps1
@@ -5,6 +5,23 @@ function Show-OpenFileDialog {
     We'll see many examples of OpenFileDialog using Windows Forms, but we'll try to
     get off Windows Forms as it's legacy tech.
 
+.EXAMPLE
+    PS_>Show-OpenFileDialog
+    This will open a dialog box to enable the user to select a file. The output of
+    the function is the full path of that file. It's useful for example to Import-CSV
+    from a CSV file, or to select an Office document to be opened with PowerShell
+    application automation...
+
+.EXAMPLE
+    PS_>$FileName = Show-OpenFileDialog
+    This will open a dialog box to enable the user to select a file, and the file name
+    will be stored in the $FileName variable to be reused as described on the first example.
+
+.EXAMPLE
+    PS_>$FileName = Show-OpenFileDialog -Title "Open an .XLSX file to be parsed" -Filter "Excel file|*.xlsx"  -InitialDirectory c:\MyExcelFiles
+    This will open a dialog box to select a file, with a customized title, and with a default filter on *.xlsx Excel files. This
+    dialog box will open the C:\MyExcelFiles directory to look for files. User can select later any other folder to look for.
+
 .LINK
     https://docs.microsoft.com/en-us/dotnet/api/microsoft.win32?view=net-5.0
 
